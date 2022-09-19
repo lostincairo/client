16d34b58bc0c209ecbfa8884850895dc3ee44926
Yoann Tilmont <yoanntilmont@gmail.com>
2022-09-19 19:54:30 +0200

    further work on multi

--- components/Auth/Auth.js
+++ components/Auth/Auth.js
@@ -1,14 +1,14 @@
-
+import { react, useState } from "react";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getDatabase, set, ref, onDisconnect } from "firebase/database";
+import { getDatabase, set, ref, onDisconnect, onValue } from "firebase/database";
 import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth";
 import { data } from "autoprefixer";
+
+import Player from "../GameContainer/Player";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
-export default function AuthUser(WalletAddress = "None") {
     // Your web app's Firebase configuration
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
     const firebaseConfig = {
@@ -35,38 +35,66 @@ export default function AuthUser(WalletAddress = "None") {
     let playerRef;
 
 
+    export default function AuthUser(WalletAddress = "None") {
 
-    onAuthStateChanged(auth, (user) => {
-        console.log(user)
-        if (user) {
-            console.log("You're logged in!");
-            playerId = user.uid;
-            set(ref(db, 'players/' + playerId), {
-                id: playerId,
-                Wallet: WalletAddress
-             });
-             playerRef = 'players/' + playerId
+        onAuthStateChanged(auth, (user) => {
+            console.log(user)
+            if (user) {
+                console.log("You're logged in!");
+                playerId = user.uid;
+                set(ref(db, 'players/' + playerId), {
+                    id: playerId,
+                    Wallet: WalletAddress
+                 });
+                 playerRef = 'players/' + playerId
+    
+                onDisconnect(ref(db, 'players/' + playerId)).remove();
+    
+            } else {
+                console.log("You're logged out.");
+            }
+        });
+    
+    
+    
+         signInAnonymously(auth).catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+            console.log(errorCode + " " + errorMessage);
+        });
+    
+        return (
+            <div className="bg-white">
+                <h1 className="text-black">This is Auth</h1>
+            </div>
+        )
+    }
+    
+    
 
-            onDisconnect(ref(db, 'players/' + playerId)).remove();
+export function InitGame() {
 
-        } else {
-            console.log("You're logged out.");
+    // Manage State
+    const [showPlayers, setShowPlayers] = useState(false);
+
+    const allPlayersRef = ref(db, 'players/' + playerId)
+
+    onValue( allPlayersRef, (snapshot) => {
+        // fires whenever a change occurs in the db
+        const addedPlayer = snapshot.val();     
+        // setShowPlayers(false);
+        
+        if (addedPlayer.id === playerId) {
+
         }
-    });
 
-
-
-     signInAnonymously(auth).catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-        console.log(errorCode + " " + errorMessage);
-    });
+    })
 
     return (
-        <div className="bg-white">
-            <h1 className="text-black">This is Auth</h1>
-        </div>
-    )
-}
+        <Player show={showPlayers} />
+    );
+
+    }
+
 
--- components/Navigation.js
+++ components/Navigation.js
@@ -1,7 +1,8 @@
 import Link from "next/link"
 import { useState } from 'react'
 import Home from "./Home"
-import Map from "./GameContainer/Map"
+import InitMap from "./GameContainer/Map"
+import { InitGame } from "./Auth/Auth"
 
 
 export default function Navigation() {
@@ -11,6 +12,7 @@ export default function Navigation() {
     return (
       <div className="flex justify-center content-center mx-auto max-w-7xl h-screen w-full p-5 flex-col lg:px-8">
         <Home />
+        <InitGame />
         </div>
     );
 }
