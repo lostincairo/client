61b152cc7991f2d7c0c8e47622787d37c8896d0b
Yoann Tilmont <yoanntilmont@gmail.com>
2022-10-10 11:48:35 +0200

    starknet slice

---
+++ components/UI/Move.js
@@ -0,0 +1,37 @@
+import { useSelector } from "react-redux";
+import {
+  useStarknetInvoke, 
+  useAccount,
+} from "@starknet-react/core";
+import { useGameContract } from "/hooks/GameContract";
+
+
+export default function EndTurn() {
+
+    const { selectedCell } = useSelector((store) => store._scene)
+
+  const { address } = useAccount();
+  const { contract: game } = useGameContract();
+  const { data, loading, invoke } = useStarknetInvoke({
+    contract: game,
+    method: "end_turn"
+});
+
+
+const call_end_turn = () => {
+  invoke({
+    args:[1, 0x1],
+  });
+};
+
+
+    return (
+        <div className="flex justify-center content-center max-w-7xl w-full p-5 flex-col lg:px-8">
+          <button
+            type="button"
+            className="mx-auto w-40 h-20 hover:bg-[url('/play_button_hover.svg')] bg-[url('/play_button.svg')] bg-contain bg-no-repeat bg-center px-4 py-4" 
+            onClick={call_end_turn}
+          ></button>
+        </div>
+      );
+}
--- redux/starknetSlice.js
+++ redux/starknetSlice.js
@@ -1,6 +1,10 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 const initialState = {
+  isYourTurn: false,
+  selectedAction: null,
+  playerPosition: [],
+  playerDirection: null,
 }
 
 
@@ -8,10 +12,22 @@ export const starknetSlice = createSlice({
   name: "_starknet",
   initialState,
   reducers: {
+    turn: (state, action) => {
+      state.isYourTurn = action.payload;
+  },
+    action: (state, action) => {
+      state.selectedAction = action.payload;
+  },
+  position: (state, action) => {
+      state.playerPosition = action.payload;
+  },
+  direction: (state, action) => {
+      state.playerDirection = action.payload;
+  }
   }
 });
 
 // Action creators are generated for each case reducer function
-export const { selectCell, highlightCell, movePlayer } = starknetSlice.actions
+export const { turn, action, position, direction } = starknetSlice.actions
 
 export default starknetSlice.reducer
